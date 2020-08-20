8efb1965e57f6ac27e1057b99042de51c612a313
Gunx <gunx@foxmail.com>
2020-08-20 17:36:08 +0800

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ function App ({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#202020" />
+        <meta name="description" content="Echoes from retrofuturism" />
         <title>ViLAWAVE</title>
       </Head>
       <Component {...pageProps} />
